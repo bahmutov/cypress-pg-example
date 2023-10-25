3e6a3a76a162441629fe69fdef70f93c4a43f012
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-10-25 19:43:16 -0400

    o6

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,32 +6,24 @@ describe('Postgres database', () => {
     // by calling task "query"
     // with the string argument
     // "truncate table messages"
-    cy.task('query', 'truncate table messages')
   })
 
   it('adds a new message', () => {
     // create a random string to be the message
     // Tip: use Cypress._.random
-    const message = `message ${Cypress._.random(1e6)}`
     // send the message to the server
     // using "POST /messages" API call
     // https://on.cypress.io/request
-    cy.request('POST', '/messages', { message })
+    //
     // get all messages from the database
     // and confirm the list includes the new message
     // use the query "select message from messages"
     // which should return a list like [ {message: }, {message: }, ...]
-    cy.task('query', 'select message from messages').should('deep.include', {
-      message,
-    })
+    //
     // select only the message equal to the given random message
     // and confirm there is only one found record
     // you will need to pass both the query
     // "select message from messages where message = $1"
     // and its params [message]
-    cy.task('query', {
-      query: 'select message from messages where message = $1',
-      params: [message],
-    }).should('deep.equal', [{ message }])
   })
 })
