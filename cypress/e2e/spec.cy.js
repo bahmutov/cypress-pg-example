249d8ca6a4ca1ddf85aa73130685929b7ebee977
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-10-30 11:15:59 -0400

    o8 solution

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,4 @@
-/// <reference types="cypress" />
+/// <reference path="../support/index.d.ts" />
 
 describe('Postgres database', () => {
   beforeEach(() => {
@@ -9,28 +9,21 @@ describe('Postgres database', () => {
     cy.query('truncate table messages')
   })
 
-  it('adds a new message', () => {
-    // create a random string to be the message
-    // Tip: use Cypress._.random
-    const message = `message ${Cypress._.random(1e6)}`
-    // send the message to the server
-    // using "POST /messages" API call
-    // https://on.cypress.io/request
-    cy.request('POST', '/messages', { message })
-    // get all messages from the database
-    // and confirm the list includes the new message
-    // use the query "select message from messages"
-    // which should return a list like [ {message: }, {message: }, ...]
-    cy.query('select message from messages').should('deep.include', {
-      message,
+  it('confirms the number of messages', () => {
+    // confirm the messages table haz zero records
+    // tip: PG Count(*) function returns a single row
+    // with {count: "N"} object
+    cy.query('select count(*) from messages').should('deep.equal', [
+      { count: '0' },
+    ])
+    // insert 5 random messages into the table
+    // using SQL queries
+    Cypress._.times(5, (k) => {
+      cy.query('insert into messages values ($1)', [`message ${k + 1}`])
     })
-    // select only the message equal to the given random message
-    // and confirm there is only one found record
-    // you will need to pass both the query
-    // "select message from messages where message = $1"
-    // and its params [message]
-    cy.query('select message from messages where message = $1', [
-      message,
-    ]).should('deep.equal', [{ message }])
+    // confirm there are 5 messages in the table
+    cy.query('select count(*) from messages')
+      .its('0.count')
+      .should('equal', '5')
   })
 })
